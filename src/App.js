3f233e27b46ddd168c42939217f86065fef07063
will <williamcui888@hotmail.com>
2020-10-21 20:50:38 -0700

    made foundations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Drawer } from "@material-ui/core"
+import logo from './profilepicture.png';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import './App.css';
+import Socials from './components/Socials';
+
+
+const useStyles = makeStyles((theme) =>( {
+  drawerPaper: { width: "inherit" }
+
+}))
 
 function App() {
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Drawer
+          style={{ width: '200px' }}
+          variant="persistent"
+          anchor="left"
+          open={false}
+          classes={{ paper: classes.drawerPaper }}>
+
+        </Drawer>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <body className="App-link">
+            William Cui
+        </body>
+
+          <Socials />
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Socials.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+                <a href="https://github.com/spyrux" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://Linkedin.com/in/wcuidev" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://Instagram.com/ypyru" target="_blank"><i class="fab fa-instagram"></i></a>
+            </div>
+        )
+    }
+}
+export default Social
